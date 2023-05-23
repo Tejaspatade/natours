a4fdd16f7d7f1619ec243b89559e3df2c7b48437
Tejas Patade <tej.patade69@gmail.com>
2023-05-23 18:55:40 +0530

    Installed JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,13 +3,18 @@ const catchAsync = require("../utils/catchAsync");
 
 // Sign Up New User with credentials
 exports.signUp = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
 
-    // 201: Created
-    res.status(201).json({
-        status: "success",
-        data: {
-            user: newUser,
-        },
-    });
+	// 201: Created
+	res.status(201).json({
+		status: "success",
+		data: {
+			user: newUser,
+		},
+	});
 });
