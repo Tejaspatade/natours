232977cb6a08baca8b8d3dcefdb90addb0f0e9d5
Tejas Patade <tej.patade69@gmail.com>
2023-05-30 14:21:28 +0530

    Imported Full Tour Data for Modelling

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,86 +3,111 @@ const mongoose = require("mongoose");
 
 // Tour Schema
 const tourSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "A tour must have a name"],
-            unique: true,
-            trim: true,
-            maxLength: [40, "A tour name can't be larger than  40 characters."],
-            minLength: [10, "A tour name must have atleast 10 characters."],
-            // validate: [
-            //     validator.isAlpha,
-            //     "Tour Name must only contain characters.",
-            // ],
-        },
-        duration: {
-            type: Number,
-            required: [true, "A tour must have a duration"],
-        },
-        maxGroupSize: {
-            type: Number,
-            required: [true, "A tour must have a group size"],
-        },
-        difficulty: {
-            type: String,
-            enum: {
-                values: ["easy", "medium", "difficult"],
-                message: "Difficulty should only be Easy, Medium or Difficult",
-            },
-        },
-        price: { type: Number, required: [true, "A tour should have price"] },
-        priceDiscount: {
-            type: Number,
-            validate: {
-                validator: function (val) {
-                    // Only works with creating new document
-                    return val < this.price;
-                },
-                message:
-                    "Discount can't be greater than or equal to the tour price.",
-            },
-        },
-        summary: {
-            type: String,
-            trim: true,
-            required: [true, "A tour must have a summary"],
-        },
-        ratingsAverage: {
-            type: Number,
-            default: 4.5,
-            min: [1, "Rating must be atleast 1.0"],
-            max: [10, "Rating can't be above 10"],
-        },
-        ratingsQuantity: { type: Number, default: 0 },
-        description: { type: String, trim: true },
-        imageCover: {
-            type: String,
-            required: [true, "A tour must have a cover image"],
-        },
-        images: [String],
-        createdAt: { type: Date, default: Date.now() },
-        startDates: [Date],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        toObject: {
-            virtuals: true,
-        },
-    }
+	{
+		name: {
+			type: String,
+			required: [true, "A tour must have a name"],
+			unique: true,
+			trim: true,
+			maxLength: [40, "A tour name can't be larger than  40 characters."],
+			minLength: [10, "A tour name must have atleast 10 characters."],
+			// validate: [
+			//     validator.isAlpha,
+			//     "Tour Name must only contain characters.",
+			// ],
+		},
+		duration: {
+			type: Number,
+			required: [true, "A tour must have a duration"],
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [true, "A tour must have a group size"],
+		},
+		difficulty: {
+			type: String,
+			enum: {
+				values: ["easy", "medium", "difficult"],
+				message: "Difficulty should only be Easy, Medium or Difficult",
+			},
+		},
+		price: { type: Number, required: [true, "A tour should have price"] },
+		priceDiscount: {
+			type: Number,
+			validate: {
+				validator: function (val) {
+					// Only works with creating new document
+					return val < this.price;
+				},
+				message:
+					"Discount can't be greater than or equal to the tour price.",
+			},
+		},
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, "A tour must have a summary"],
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5,
+			min: [1, "Rating must be atleast 1.0"],
+			max: [10, "Rating can't be above 10"],
+		},
+		ratingsQuantity: { type: Number, default: 0 },
+		description: { type: String, trim: true },
+		imageCover: {
+			type: String,
+			required: [true, "A tour must have a cover image"],
+		},
+		images: [String],
+		createdAt: { type: Date, default: Date.now() },
+		startDates: [Date],
+		startLocation: {
+			// This field is an object with its own fields
+			// GeoJSON
+			type: {
+				type: String,
+				default: "Point",
+				enum: ["Point"],
+			},
+			coordinates: [Number],
+			address: String,
+			description: String,
+		},
+		locations: [
+			{
+				type: {
+					type: String,
+					default: "Point",
+					enum: ["Point"],
+				},
+				coordinates: [Number],
+				address: String,
+				description: String,
+				day: Number,
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		toObject: {
+			virtuals: true,
+		},
+	}
 );
 
 // Virtual Properties
 tourSchema.virtual("durationWeeks").get(function () {
-    // Used Regular function rather than arrow func bcuz we need this to refer to props from document
-    return this.duration / 7;
+	// Used Regular function rather than arrow func bcuz we need this to refer to props from document
+	return this.duration / 7;
 });
 
 // Document Middleware
 tourSchema.pre("save", function (next) {
-    next();
+	next();
 });
 
 // Model using Schema
