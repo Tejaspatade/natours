3a57f77558a5c739a22264965c72b77aff9cdea5
Tejas Patade <tej.patade69@gmail.com>
2023-05-31 20:12:04 +0530

    Imported Users & Reviews

--- models/userModel.js
+++ models/userModel.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
 	},
 	role: {
 		type: String,
-		enum: ["user", "admin", "guide"],
+		enum: ["user", "admin", "guide", "tour-guide"],
 		default: "user",
 	},
 	photo: {
