1c26322a1707ce30fcd4ce964cfa045045220eff
Tejas Patade <tej.patade69@gmail.com>
2023-08-16 00:31:36 +0530

    Cors Config

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.use(
 	})
 );
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
